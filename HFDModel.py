ebc6c5a623604b85e2805124f06547ed2d8df9a5
Liwen-archer <cs888song@gmail.com>
2025-07-12 20:17:50 +0800

    code finished

---
+++ HFDModel.py
@@ -0,0 +1,113 @@
+import torch
+import torch.nn as nn
+import math
+import transformer
+
+
+class PositionalEncoding(nn.Module):
+
+    def __init__(self, seq_len, d_model, dropout=0.1):
+        super(PositionalEncoding, self).__init__()
+        max_len = max(5000, seq_len)
+        self.dropout = nn.Dropout(p=dropout)
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(
+            0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+
+        if d_model % 2 == 0:
+            pe[:, 1::2] = torch.cos(position * div_term)
+        else:
+            pe[:, 1::2] = torch.cos(position * div_term)[:, 0: -1]
+
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer('pe', pe)
+
+    # Input: seq_len x batch_size x dim, Output: seq_len, batch_size, dim
+    def forward(self, x):
+        x = x + self.pe[:x.size(0), :]
+        return self.dropout(x)
+    
+    
+class Permute(torch.nn.Module):
+    def forward(self, x):
+        return x.permute(1, 0)
+    
+    
+class HFDModel(nn.Module):
+    def __init__(self, task_type, device, nclasses, seq_len, batch, input_size, emb_size, nhead, nhid, nhid_tar, nhid_task, nlayers, dropout=0.1):
+        super(HFDModel, self).__init__()
+        
+        self.trunk_net = nn.Sequential(
+            nn.Linear(input_size, emb_size),
+            nn.BatchNorm1d(batch),
+            PositionalEncoding(seq_len, emb_size, dropout),
+            nn.BatchNorm1d(batch)
+        )
+        
+        input_size_2 = int(input_size / 2)
+        self.trunk_net2 = nn.Sequential(
+            nn.Linear(input_size_2, emb_size),
+            nn.BatchNorm1d(batch),
+            PositionalEncoding(seq_len, emb_size, dropout),
+            nn.BatchNorm1d(batch)
+        )
+        
+        encoder_layers = transformer.TransformerEncoderLayer(emb_size, nhead, nhid, dropout)
+        self.transformer_encoder = transformer.TransformerEncoder(encoder_layers, nlayers, device)
+        
+        self.batch_norm = nn.BatchNorm1d(batch)
+        
+        self.tar_net = nn.Sequential(
+            nn.Linear(emb_size, nhid_tar),
+            nn.BatchNorm1d(batch),
+            nn.Linear(nhid_tar, nhid_tar),
+            nn.BatchNorm1d(batch),
+            nn.Linear(nhid_tar, input_size),
+        )
+        
+        self.class_net = nn.Sequential(
+            nn.Linear(emb_size, nhid_task),
+            nn.ReLU(),
+            Permute(),
+            nn.BatchNorm1d(batch),
+            Permute(),
+            nn.Dropout(p = 0.3),
+            nn.Linear(nhid_task, nhid_task),
+            nn.ReLU(),
+            Permute(),
+            nn.BatchNorm1d(batch),
+            Permute(),
+            nn.Dropout(p = 0.3),
+            nn.Linear(nhid_task, nclasses)
+        )
+        
+    
+    def forward(self, x, task_type):        
+        if task_type == 'reconstruction':
+            x = self.trunk_net(x.permute(1, 0, 2))
+            x, attn = self.transformer_encoder(x)
+            x = self.batch_norm(x)
+        
+            output = self.tar_net(x).permute(1, 0, 2)
+        elif task_type == 'classification':
+            x1 = x[:, :, 0:2]
+            x1 = self.trunk_net_2(x1.permute(1, 0, 2))
+            x1, attn1 = self.transformer_encoder(x1)
+            x1 = self.batch_norm(x1)
+            
+            x2 = x[:, :, 2:4]
+            x2 = self.trunk_net_2(x2.permute(1, 0, 2))
+            x2, attn2 = self.transformer_encoder(x2)
+            x2 = self.batch_norm(x2)
+            
+            x = self.trunk_net(x.permute(1, 0, 2))
+            x, attn = self.transformer_encoder(x)
+            x = self.batch_norm(x)
+            
+            input_x = x[-1] + x1[-1] + x2[-1]
+            
+            output = self.class_net(input_x)
+        
+        return output, attn
---
+++ test_util.py
@@ -0,0 +1,51 @@
+import torch
+import math
+import logging
+
+from utils import random_instance_masking, multitask_train, test
+
+def train_model(model, optimizer, criterion_tar, criterion_task, best_model, best_optimizer, X_train_task, y_train_task, prop):
+    tar_loss_masked_arr, tar_loss_unmasked_arr, tar_loss_arr, task_loss_arr, min_task_loss = [
+    ], [], [], [], math.inf
+
+    instance_weights = torch.as_tensor(torch.rand(
+        X_train_task.shape[0], prop['seq_len']), device=prop['device'])
+    for epoch in range(1, prop['epochs'] + 1):
+
+        X_train_tar, y_train_tar_masked, y_train_tar_unmasked, boolean_indices_masked, boolean_indices_unmasked = \
+            random_instance_masking(
+                X_train_task, prop['masking_ratio'], prop['ratio_highest_attention'], instance_weights)
+
+        tar_loss_masked, tar_loss_unmasked, task_loss, instance_weights = multitask_train(model, criterion_tar, criterion_task, optimizer, X_train_tar, X_train_task, y_train_tar_masked, y_train_tar_unmasked, y_train_task, boolean_indices_masked, boolean_indices_unmasked, prop)
+
+        tar_loss_masked_arr.append(tar_loss_masked)
+        tar_loss_unmasked_arr.append(tar_loss_unmasked)
+        tar_loss = tar_loss_masked + tar_loss_unmasked
+        tar_loss_arr.append(tar_loss)
+        task_loss_arr.append(task_loss)
+        print('Epoch: ' + str(epoch) + ', TAR Loss: ' + str(tar_loss), ', TASK Loss: ' + str(task_loss))
+
+        # save model and optimizer for lowest training loss on the end task
+        if task_loss < min_task_loss:
+            min_task_loss = task_loss
+            best_model.load_state_dict(model.state_dict())
+            best_optimizer.load_state_dict(optimizer.state_dict())
+
+    # Saved best model state at the lowest training loss is evaluated on the official test set
+    metrics = test(best_model, X_train_task, y_train_task, prop['batch'], prop['nclasses'], criterion_task, prop['task_type'], prop['device'], prop['avg'])
+    print('TRAIN ===> Dataset: ' + prop['dataset'] + ', Acc: ' + str(metrics[1]) )
+
+    del model
+    torch.cuda.empty_cache()
+    
+
+def test_model(model, criterion_task, X_test, y_test, prop):
+    test_metrics = test(model, X_test, y_test, prop['batch'], prop['nclasses'], criterion_task, prop['task_type'], prop['device'], prop['avg'])
+
+    loss, acc, prec, rec, f1 = test_metrics
+    print('Test: ' + prop['dataset'], ', Loss: ' + str(loss), ', Acc: ' + str(acc), ', Prec: ' + str(prec), ', Rec: ' + str(rec), ', F1: ' + str(f1))
+    logging.info(f"Test {prop['dataset']}, {acc}, {prec}, {rec}, {f1}")
+
+
+    del model
+    torch.cuda.empty_cache()
